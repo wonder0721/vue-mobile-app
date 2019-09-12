1b0e42c0470369ba71c6d06cdc3552a2435f8a4a
wonder0721 <2245583186@qq.com>
2019-09-12 19:51:55 +0800

    数字计数器组件

---
+++ src/components/inputnumber/inputnumber.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mui-numbox" data-numbox-min='1' data-numbox-max='10' style="width:105px;height:25px">
+        <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+        <!-- 记得要将父组件传递过来的值用属性绑定的方式调用v-bind -->
+        <input id="test" @change="countChange" ref="numBox" class="mui-input-numbox" type="number" :value="goodsCount" readonly/>
+        <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+    </div>
+</template>
+
+<script>
+import mui from '../../lib/mui/js/mui.js'
+export default {
+    props: ['goodsCount','goodsid'],
+    mounted() {
+        mui('.mui-numbox').numbox()
+    },
+    methods:{
+        countChange(){
+            // 通过父向子传递方法，子调用方法并传递参数，父获取到子项的数据
+            console.log(typeof(this.$refs.numBox.value))
+            this.$store.commit('updateGoodsCount',{id:this.goodsid,count:parseInt(this.$refs.numBox.value)})
+        }
+    },   
+}
+</script>
+
+style <style lang="scss" scoped>
+
+</style>
