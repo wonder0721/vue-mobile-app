6831dba363322cef0cb3cc0cf9599f87c51217d7
wonder0721 <2245583186@qq.com>
2019-09-12 19:48:14 +0800

    轮播图组件

---
+++ src/components/swiper/swiper.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <mt-swipe :auto="5000">
+            <mt-swipe-item v-for="item in imageList" :key="item.id">
+                <img :src="item.img" alt="" :class="{'full':fullwidth}">
+            </mt-swipe-item>
+        </mt-swipe>
+    </div>
+</template>
+
+<script>
+import { Swipe, SwipeItem } from 'mint-ui';
+export default {
+    props: ['imageList','fullwidth'],
+}
+</script>
+
+<style lang="scss" scoped>
+.mint-swipe{
+    height:200px;
+}
+.mint-swipe img{
+    height:100%;
+}
+.full{
+    width:100%;
+}
+</style>
