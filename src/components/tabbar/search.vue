e5f5230950631809341a1b58c3a0145309117733
wonder0721 <2245583186@qq.com>
2019-09-12 19:48:49 +0800

    搜索组件

---
+++ src/components/tabbar/search.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="search">
+        <div>
+            <input type="text" placeholder="输入想搜索的内容" style="width:230px;height:35px;">
+            <button class="btn btn-info">
+                <span class="glyphicon glyphicon-search"></span>&nbsp;搜索
+            </button>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            value: '',
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.search{
+    padding:5px;
+}
+</style>>
